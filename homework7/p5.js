cded279f08c744d5562e063b89c0ce8daecb8d0f
hahyoungkim <hyk323@nyu.edu>
2022-11-07 18:27:08 -0500

    stars

--- homework7/p5.js
+++ homework7/p5.js
@@ -1,13 +1,42 @@
-function setup() {
-    createCanvas(windowWidth, windowHeight);  
-    colorMode(HSB); // rainbow color
-    strokeWeight(20);
+function setup(){
+    createCanvas(windowWidth,windowWidth);
 }
-  
-function draw() {
-    stroke((5*frameCount) % 360, 40, 100);
-    fill((5*frameCount) % 360, 100, 100);
-    line(mouseX, mouseY, pmouseX, pmouseY);
+
+function mouseClicked(){
+    star();
+}
+
+function mouseDragged(){
+    star();
+}
+
+function star() {
+	beginShape();  
     
+    pastelColor = color(random(150,255),random(150,255),random(150,255));
+    fill(pastelColor);
+    stroke(pastelColor)
+
+    x = mouseX;
+    y = mouseY;
+    size = random(1,4);
+	vertex(x+0/size, y-50/size);
+	vertex(x+14/size, y-20/size);
+	vertex(x+47/size, y-15/size);
+	vertex(x+23/size, y+7/size);
+	vertex(x+29/size, y+40/size);
+	vertex(x+0/size, y+25/size);
+	vertex(x-29/size, y+40/size);
+	vertex(x-23/size, y+7/size);
+	vertex(x-47/size, y-15/size);
+	vertex(x-14/size, y-20/size);
+	
+    endShape(CLOSE);
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  setRadius();
 }
 

a7b54f8fc6ff6558515be7326dd3b887124d08be
hahyoungkim <hyk323@nyu.edu>
2022-11-17 13:07:00 -0500

    animation

--- homework9/medium.js
+++ homework9/medium.js
@@ -7,10 +7,14 @@ insta_animation.style.visibility = "hidden";
 
 //create an event listener for upon click
 insta_button.addEventListener('click', ig_like)
-
+insta_animation.addEventListener('click', ig_reverse)
 
 function ig_like(){
     insta_button.style.visibility = "hidden";
     insta_animation.style.visibility = "visible";
 }
 
+function ig_reverse(){
+    insta_button.style.visibility = "visible";
+    insta_animation.style.visibility = "hidden";
+}
